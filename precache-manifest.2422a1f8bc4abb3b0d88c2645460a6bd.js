6339c56e908a15ec10093761aeed9247b795e1b4
Sergio <sergio.sp@protonmail.com>
2021-05-09 11:11:19 -0500

    Updates

---
+++ precache-manifest.2422a1f8bc4abb3b0d88c2645460a6bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6116c16c75c801d328e1eb5107a8b3f7",
+    "url": "/almafound/index.html"
+  },
+  {
+    "revision": "1c6f5079c0963809f12b",
+    "url": "/almafound/static/css/main.8061f892.chunk.css"
+  },
+  {
+    "revision": "142d17b57bcb344f3e94",
+    "url": "/almafound/static/js/2.64ba29d5.chunk.js"
+  },
+  {
+    "revision": "1c6f5079c0963809f12b",
+    "url": "/almafound/static/js/main.38cba644.chunk.js"
+  },
+  {
+    "revision": "2c7364a2c7c7e8b25936",
+    "url": "/almafound/static/js/runtime~main.54a4ea33.js"
+  }
+]);
